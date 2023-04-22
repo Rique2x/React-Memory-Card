6db7194ce6894ab50770500095353c1f8752c65c
Rique2 <enrique2heinrich@gmail.com>
2023-04-23 00:38:09 +0200

    Added a background video and image

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,3 @@
-module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: [
-      'plugin:react/recommended'
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    plugins: [
-      'react',
-    ],
-    rules: {
-    },
-  };
+{
+  "extends" ["react-app", "react-app/jest", "prettier"]
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index';
+import backgroundVideo from './assets/background.mp4';
+import backgroundImage from './assets/background.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Test</div>
+      <video id="background-video" autoPlay loop muted poster={backgroundImage}>
+        <source src={backgroundVideo} type="video/mp4" />
+      </video>
     </div>
   );
 }
