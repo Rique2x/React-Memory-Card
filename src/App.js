78f39f19ebb66339be9e6f0fbdad61a170417e73
Rique2 <enrique2heinrich@gmail.com>
2023-04-23 09:10:32 +0200

    Added 2 new components

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import './index';
 import backgroundVideo from './assets/background.mp4';
 import backgroundImage from './assets/background.png';
 
+import Cards from './components/Cards';
+import Scoreboard from './components/Scoreboard';
+
 function App() {
   return (
     <div className="App">
-      <div>Test</div>
+      <h1>One Piece Memory Game</h1>
+
+      <Scoreboard />
+      <Cards />
+
       <video id="background-video" autoPlay loop muted poster={backgroundImage}>
         <source src={backgroundVideo} type="video/mp4" />
       </video>
