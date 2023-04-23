427c5ddbe7df42a7e4a2cd18ef50f86868272764
Rique2 <enrique2heinrich@gmail.com>
2023-04-23 11:08:12 +0200

    Added new method and updated componenets

--- src/App.js
+++ src/App.js
@@ -5,14 +5,31 @@ import backgroundImage from './assets/background.png';
 
 import Cards from './components/Cards';
 import Scoreboard from './components/Scoreboard';
+import getCharacters from './utilities/getCharacters';
+import { useState } from 'react';
 
 function App() {
+  const [selected, setSelected] = useState([]);
+
+  const characters = getCharacters();
+
+  const onSelection = (obj) => (e) => {
+    e.preventDefault();
+
+    // If the selection already exists
+    if (selected.some((object) => object.name === obj.name)) {
+      return setSelected([]);
+    }
+
+    setSelected([...selected, obj]);
+  };
+
   return (
     <div className="App">
       <h1>One Piece Memory Game</h1>
 
       <Scoreboard />
-      <Cards />
+      <Cards characters={characters} onSelection={onSelection}/>
 
       <video id="background-video" autoPlay loop muted poster={backgroundImage}>
         <source src={backgroundVideo} type="video/mp4" />
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,12 +1,10 @@
 import React from "react";
-import getCharacters from "../utilities/getCharacters";
 
 function Cards(props) {
-  const characters = getCharacters();
 
   <div className="card-container">
-    {characters.map((obj, index) => (
-        <div key={index} className="card">
+     {props.characters.map((obj, index) => (
+        <div key={index} className="card" onClick={props.onSelection(obj)}>
           {obj.name}
         </div>
       ))}
