548895a065dede1c903152c6797076b55e35dcb5
Rique2 <enrique2heinrich@gmail.com>
2023-04-25 00:58:46 +0200

    Updated charArr

--- src/utilities/charArr.js
+++ src/utilities/charArr.js
@@ -1,19 +1,14 @@
-import placeholder from '../assets/background.png';
+function importAll(r) {
+  return r.keys().map(r);
+}
 
-const characters = [
-  { name: 'Luffy', image: placeholder },
-  { name: 'Nami', image: placeholder },
-  { name: 'Zoro', image: placeholder },
-  { name: 'Usopp', image: placeholder },
-  { name: 'Sanji', image: placeholder },
-  { name: 'Chopper', image: placeholder },
-  { name: 'Robin', image: placeholder },
-  { name: 'Franky', image: placeholder },
-  { name: 'Brook', image: placeholder },
-  { name: 'Jinbe', image: placeholder },
-  { name: 'Vivi', image: placeholder },
-  { name: 'Zeus', image: placeholder },
-  { name: 'Karoo', image: placeholder },
-  ];
+const images = importAll(require.context('../assets/wanted-images/', false, /\.(webp)$/));
   
-  export default characters;
+const charArr = images.map((path) => {
+  return {
+    name: path.split(/[., /]/)[3],
+    image: path,
+  };
+});
+
+export default charArr;
